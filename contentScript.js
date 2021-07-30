afcf4b37882c98e61120f25de804e2b7a0c6a20b
Blair McMillan <blair@mcmillan.id.au>
2021-07-30 10:54:05 +1000

    Better handling for getting patreon post data

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,19 @@
 try {
   chrome.runtime.sendMessage({type: 'whoAmI'}, tabId => {
-    const payload = '{'.concat(document.documentElement.innerHTML.split('Object.assign(window.patreon.bootstrap, {')[1].split(');')[0]);
-    const data = {};
-    data[tabId.tab.toString()] = JSON.parse(payload);
-    chrome.storage.local.set(data, function () {
-      if (chrome.runtime.lastError) {
-        console.error('Patreon Downloader | Failed to set data for tab.', tabId.tab, data, chrome.runtime.lastError.message);
-      } else {
-        console.debug('Patreon Downloader | Set page data.', tabId.tab, data);
-      }
-    });
+    try {
+      const payload = '{'.concat(document.documentElement.innerHTML.split('Object.assign(window.patreon.bootstrap, {')[1].split('\n});\n      Object.assign(window.patreon')[0], '}');
+      const data = {};
+      data[tabId.tab.toString()] = JSON.parse(payload);
+      chrome.storage.local.set(data, function () {
+        if (chrome.runtime.lastError) {
+          console.error('Patreon Downloader | Failed to set data for tab.', tabId.tab, data, chrome.runtime.lastError.message);
+        } else {
+          console.debug('Patreon Downloader | Set page data.', tabId.tab, data);
+        }
+      });
+    } catch (e) {
+      console.error('Patreon Downloader |', e);
+    }
   });
 } catch (e) {
   console.error('Patreon Downloader |', e);
